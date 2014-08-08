b8ba277cb49a33b7c0a4de099e5284198374d6fd
Rashid Goshtasbi <RashidGoshtasbi@ucrwpa3-1-209-123.bulk.ucr.edu>
2014-08-08 11:55:49 -0700

    make rshell loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,13 @@
 #include <string>
 #include <sstream>
 #include <unistd.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <vector>
 
 using namespace std;
 int main()
 {
-	int child_pid;  
-	int parent_pid = getppid();
-	int status = 0;
 	istringstream iss;
 	string usrinput;
 
@@ -18,113 +16,114 @@ int main()
 	char hostname[128];
 	gethostname(hostname, sizeof hostname);
 
-	//prints out username of current user along with local machine name
-	cout << getlogin() << "@" << hostname << "$ ";
-
-	getline(cin, usrinput);
-	while (usrinput == "")
+	bool runShell = true;
+	while(runShell)
 	{
+		istringstream iss;
+		//prints out username of current user along with local machine name
 		cout << getlogin() << "@" << hostname << "$ ";
+
 		getline(cin, usrinput);
-	}
-	//function finds where the '#' index is at
-	int poundIndex = -1;
-	for (int i = 0; i < usrinput.size(); ++i)
-	{
-		if(usrinput.at(i) == '#')
+		while (usrinput == "")
 		{
-			poundIndex = i;
-			break;
+			cout << getlogin() << "@" << hostname << "$ ";
+			getline(cin, usrinput);
 		}
-	}
 
-	//takes poundIndex and uses the erase function to remove the comments
-	//so that the program will take in the command name and arguement(s)
-	if (poundIndex != -1)
-	{
-		usrinput.erase(poundIndex, usrinput.size());
-	}
-	
-	//separates the strings into the vector
-	iss.str(usrinput);
-	string val;
-	vector<string> commands;
-	while(iss >> val)
-	{
-		if (val == "exit")
+		if (usrinput == "exit")
 		{
-			return 0;
+			exit(0);
 		}
-		commands.push_back(val);
-	}
-	if (commands.size() == 0)
-	{
-		cout << "Error: Input was empty." << endl;
-		return 0;
-	}
-	//allocating memory for the array
-	int size = commands.size();
-	char** cp;
-	cp = new char*[size * sizeof(char*)];
-	
-	//copies command and argument(s) to array for EXECV()
-	for (int i = 0; i < size; ++i)
-	{
-		string str2 = commands.at(i);
 
-		if (i == 0)
+		//function finds where the '#' index is at
+		int poundIndex = -1;
+		bool andSign = false;
+		for (int i = 0; i < usrinput.size(); ++i)
 		{
-			string str1 = "/bin/";
-			str2 = str1.append(str2);
+			if(usrinput.at(i) == '#')
+			{
+				poundIndex = i;
+				break;
+			}
+			if (usrinput.at(i) == '&')
+			{
+				andSign = true;
+				break;
+			}
+		}
+
+		//takes poundIndex and uses the erase function to remove the comments
+		//so that the program will take in the command name and arguement(s)
+		if (poundIndex != -1)
+		{
+			usrinput.erase(poundIndex, usrinput.size());
 		}
 		
-		cp[i] = new char[str2.length() + 1];
-		strcpy(cp[i], str2.c_str());
-	}
-	cp[size] = new char[8];
-	cp[size] = NULL;
-	//checks for array values
-	/*for (int i = 0; i < size; ++i)
-	{
-		cout << "Array at " << i << ": " << cp[i] << endl;
-	}*/
-
-	//start of the forking process
-	child_pid = fork();
-	if (child_pid == 0)
-	{
-		//child process
-		if (execv(cp[0], cp) == -1)
+		//separates the strings into the vector
+		iss.str(usrinput);
+		string val;
+		vector<string> commands;
+		while(iss >> val)
 		{
-			perror("EXECLV FAILED: ");
+			commands.push_back(val);
+		}
+		//allocating memory for the array
+		int size = commands.size();
+		char** cp;
+		cp = new char*[size * sizeof(char*)];
+		
+		//copies command and argument(s) to array for EXECV()
+		for (int i = 0; i < size; ++i)
+		{
+			string str2 = commands.at(i);
 
+			if (i == 0)
+			{
+				string str1 = "/bin/";
+				str2 = str1.append(str2);
+			}
+			
+			cp[i] = new char[str2.length() + 1];
+			strcpy(cp[i], str2.c_str());
+		}
+		cp[size] = new char[8];
+		cp[size] = NULL;
+		//checks for array values
+		/*for (int i = 0; i < size; ++i)
+		{
+			cout << "Array at " << i << ": " << cp[i] << endl;
+		}*/
+
+		pid_t child_pid = fork();
+		if (child_pid == 0)
+		{
+			//child process
+			if (execv(cp[0], cp) == -1)
+			{
+				perror("EXECLV FAILED: ");
+				for (int i = 0; i < size; ++i)
+				{
+					delete[] cp[i];
+				}
+				delete [] cp;
+				exit(0);
+			}
 			for (int i = 0; i < size; ++i)
 			{
 				delete[] cp[i];
 			}
 			delete [] cp;
-
-			exit(0);
 		}
 		else
 		{
-			execv(cp[0], cp);
+			//parent process, must wait for child process to finish
+			wait(0);
 			for (int i = 0; i < size; ++i)
 			{
 				delete[] cp[i];
 			}
 			delete [] cp;
 		}
-	}
-	else
-	{
-		wait(&status);
-		for (int i = 0; i < size; ++i)
-		{
-			delete[] cp[i];
-		}
-		delete [] cp;
-		exit(0);
 	}
 	return 0;
 }
