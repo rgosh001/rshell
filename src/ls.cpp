db36e5a5f1f71a423af2ef1afb6a415b15d766d1
Rashid Goshtasbi <RashidGoshtasbi@Rashids-MacBook-Pro.local>
2014-08-10 11:02:51 -0700

    added usr input, istringstream, and input into array

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,14 +1,99 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
 
-#include <iostream>
-
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-   char *dirName = ".";
+
+   string usrinput;
+   
+   bool runShell = true;
+   while(runShell)
+   {
+      istringstream iss;
+
+      //getting user input from the command line and will
+      //stream it into a vector
+      getline(cin, usrinput);
+
+      //need to get rid of "#" for comments
+      int poundIndex = false;
+
+      //need to check for "&" for background process
+      int andSignIndex;
+      int andSign = false;
+      for(int i = 0; i < usrinput.size(); ++i)
+      {
+         if(usrinput.at(i) == '#')
+         {
+            poundIndex = i;
+            break;
+         }
+         if (usrinput.at(i) == '&')
+         {
+            andSign = true;
+            andSignIndex = i;
+            break;
+         }
+
+      }
+
+      //takes poundIndex and uses the erase function to remove the comments
+      if(poundIndex != false)
+      {
+         usrinput.erase(poundIndex, usrinput.size());
+      }
+      
+      //removed "&" so that the command line doesn't produce an error
+      if (andSign)
+      {
+         exit(0);
+      }
+
+      //separates the strings into the vector
+      iss.str(usrinput);
+      string val;
+      vector<string> commands;
+      while(iss >> val)
+      {
+         commands.push_back(val);
+      }
+
+      //allocating memory for the array
+      int size = commands.size();
+      char ** arr;
+      arr = new char*[size * sizeof(char*)];
+
+      //copies command and argument(s) to array
+      for(int i = 0; i < size; ++i)
+      {
+         string str = commands.at(i);
+         if(i == 0)
+         {
+            string str1 = "/bin/";
+            str = str1.append(str);
+         }
+         arr[i] = new char[str.length() +1];
+         strcpy(arr[i], str.c_str());
+      }
+
+      arr[size] = new char[8];
+      arr[size] = NULL;
+
+
+      
+   }
+
+/*   char *dirName = "ls";
    DIR *dirp;
    if (!(dirp  = opendir(dirName)))
     {
@@ -18,6 +103,6 @@ int main()
    dirent *direntp;
    while ((direntp = readdir(dirp)))
        cout << direntp->d_name << endl;  // use stat here to find attributes of file
-   closedir(dirp);
+   closedir(dirp);*/
 }
 
