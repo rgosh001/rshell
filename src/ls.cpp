c6ea33970f5ec9d94daecd4924b022ac86689c6d
Rashid Goshtasbi <RashidGoshtasbi@ucrwpa3-2-213-108.bulk.ucr.edu>
2014-08-12 01:08:08 -0700

    moved output into a function and created bools for argument list

--- src/ls.cpp
+++ src/ls.cpp
@@ -13,6 +13,124 @@
 
 using namespace std;
 
+bool checkArg(vector<string>arguments, string flag)
+{
+   for(int i = 0; i < arguments.size(); ++i)
+   {
+      if (arguments.at(i) == flag)
+      {
+         return true;
+      }
+   }
+   return false;
+}
+void print(vector<string>directory, vector<string>arguments)
+{
+   bool l = checkArg(arguments, "-l");
+   bool a = checkArg(arguments, "-a");
+   bool R = checkArg(arguments, "-R");
+
+   struct stat s;
+   vector<string> files;
+   while(!directory.empty())
+   {
+      stat(directory.back().c_str(), &s);
+
+      char const *dirName = directory.back().c_str();
+      DIR *dirp;
+      if (!(dirp  = opendir(dirName)))
+       {
+          cerr << "Error(" << errno << ") opening " << dirName << endl;
+          cout << errno;
+          exit(0);
+      }
+      dirent *direntp;
+      while ((direntp = readdir(dirp)))
+      {
+         if(direntp->d_name[0] == '.' && a == false)
+         {
+            continue;
+         }
+         struct stat buf;
+         stat(direntp->d_name, &buf);
+         struct passwd *pwd;
+         if (buf.st_mode & S_IFDIR){
+            cout << 'd';
+         } else{
+            cout << '-';
+         }
+
+         if (buf.st_mode & S_IRUSR){
+            cout << 'r';
+         } else{
+            cout << '-';
+         }
+         if (buf.st_mode & S_IWUSR){
+            cout << 'w';
+         } else{
+            cout << '-';
+         }
+         if (buf.st_mode & S_IXUSR){
+            cout << 'x';
+         } else{
+            cout << '-';
+         }
+
+         if (buf.st_mode & S_IRGRP){
+            cout << 'r';
+         } else{
+            cout << '-';
+         }
+         if (buf.st_mode & S_IWGRP){
+            cout << 'w';
+         } else{
+            cout << '-';
+         }
+         if (buf.st_mode & S_IXGRP){
+            cout << 'x';
+         } else{
+            cout << '-';
+         }
+
+         if (buf.st_mode & S_IROTH){
+            cout << 'r';
+         } else{
+            cout << '-';
+         }
+         if (buf.st_mode & S_IWOTH){
+            cout << 'w';
+         } else{
+            cout << '-';
+         }
+         if (buf.st_mode & S_IXOTH){
+            cout << 'x';
+         } else{
+            cout << '-';
+         }
+         cout << " ";
+         
+         //stdout usrname
+         pwd = getpwuid(buf.st_uid);
+         cout << pwd->pw_name << " ";
+
+         //stdout the time
+         char buff[20];
+         struct tm * timeinfo;
+         timeinfo = localtime (&(buf.st_mtime));
+         strftime(buff, sizeof(buff), "%b %d %H:%M", timeinfo);
+         cout << buff << " ";
+
+         //stdout the name of the file
+         cout << direntp->d_name << endl;
+         files.push_back(direntp->d_name);
+      }
+      closedir(dirp);
+      
+      directory.pop_back();
+      cout << endl;
+   }
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -85,110 +203,10 @@ int main(int argc, char* argv[])
       cout << "Directory " << i << ": " << directory.at(i) << endl;
    }
 
-
-
-
-
-
    cout << "\t-----FILE OUTPUT AREA-----\t" << endl;
 
-
-
-
-
+   print(directory, arguments);
    
-   struct stat s;
-   vector<string> files;
-   while(!directory.empty())
-   {
-      stat(directory.back().c_str(), &s);
-
-      char const *dirName = directory.back().c_str();
-      DIR *dirp;
-      if (!(dirp  = opendir(dirName)))
-       {
-          cerr << "Error(" << errno << ") opening " << dirName << endl;
-          return errno;
-      }
-      dirent *direntp;
-      while ((direntp = readdir(dirp)))
-      {
-         struct stat buf;
-         stat(direntp->d_name, &buf);
-         if (buf.st_mode & S_IFDIR){
-            cout << 'd';
-         } else{
-            cout << '-';
-         }
-
-         if (buf.st_mode & S_IRUSR){
-            cout << 'r';
-         } else{
-            cout << '-';
-         }
-         if (buf.st_mode & S_IWUSR){
-            cout << 'w';
-         } else{
-            cout << '-';
-         }
-         if (buf.st_mode & S_IXUSR){
-            cout << 'x';
-         } else{
-            cout << '-';
-         }
-
-         if (buf.st_mode & S_IRGRP){
-            cout << 'r';
-         } else{
-            cout << '-';
-         }
-         if (buf.st_mode & S_IWGRP){
-            cout << 'w';
-         } else{
-            cout << '-';
-         }
-         if (buf.st_mode & S_IXGRP){
-            cout << 'x';
-         } else{
-            cout << '-';
-         }
-
-         if (buf.st_mode & S_IROTH){
-            cout << 'r';
-         } else{
-            cout << '-';
-         }
-         if (buf.st_mode & S_IWOTH){
-            cout << 'w';
-         } else{
-            cout << '-';
-         }
-         if (buf.st_mode & S_IXOTH){
-            cout << 'x';
-         } else{
-            cout << '-';
-         }
-
-         cout << " ";
-         
-         cout << getpwuid(buf.st_uid)<< " ";
-
-         //stdout the time
-         char buff[20];
-         struct tm * timeinfo;
-         timeinfo = localtime (&(buf.st_mtime));
-         strftime(buff, sizeof(buff), "%b %d %H:%M", timeinfo);
-         cout << buff << " ";
-
-         //stdout the name of the file
-         cout << direntp->d_name << endl;
-         files.push_back(direntp->d_name);
-      }
-      closedir(dirp);
-      
-      directory.pop_back();
-      cout << endl;
-   }
    return 0;
 }
 
