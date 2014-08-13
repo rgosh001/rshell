5eeda0d66f9605c9b2706e87470da4e73ee1fb38
Rashid Goshtasbi <RashidGoshtasbi@ucrwpa2-2-175-76.bulk.ucr.edu>
2014-08-13 13:08:34 -0700

    start of hw3, made pipe.cpp and begining main

---
+++ src/pipe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sstream>
+
+#include <unistd.h>
+#include <sys/type.h>
+#include <stdio.h>
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+
+
+   return 0;
+}
